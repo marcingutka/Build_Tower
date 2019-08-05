f855b095e9635635b954f72a034799c1b2835f12
macingutka <gucios17@gmail.com>
2019-08-05 17:42:22 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace Build_Tower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = TowerBuilder(10);
+            foreach(string o in s)
+            {
+                Console.WriteLine(o);
+            }
+        }
+
+        public static string[] TowerBuilder(int nFloors)
+        {
+            return Enumerable.Range(1, nFloors).Select(i => new string(' ', nFloors - i) + new string('*', 2 * i - 1) + new string(' ', nFloors - i)).ToArray();
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,12 @@
+Build Tower by the following given argument:
+number of floors (integer and always greater than 0).
+
+Tower block is represented as *
+
+for example, a tower of 3 floors looks like below
+
+[
+  '  *  ', 
+  ' *** ', 
+  '*****'
+]
